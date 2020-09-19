fc689c4876490fc37da3a330f9957ed626d4f111
Lucas Jesus <cc19372@g.unicamp.br>
2020-09-18 23:40:30 -0300

    need change size of header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeadComponent } from './components/head/head.component';
 import { FeedComponent } from './components/feed/feed.component';
 import { ApresentationComponentComponent } from './components/apresentation-component/apresentation-component.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadComponent,
     FeedComponent,
-    ApresentationComponentComponent
+    ApresentationComponentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
